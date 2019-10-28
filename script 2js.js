2037c8b87d81daa1456e13e295f8219cc2e0e401
Alex <ikra.sebastian@gmail.com>
2019-10-28 15:21:29 +0200

    first commit

---
+++ script 2js.js
@@ -0,0 +1,23 @@
+"use srtict";
+
+var leftBorderWith = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(pi);
+
+let rar = ["wwww.jpg" ,"rrrrr.png" ,"mac.bmp"];
+
+console.log(rar[0])
+
+// alert ('hello');
+
+confirm ("tut");
+
+//let answer = confirm("tut");
+
+//console.log(answer);
+
+let answer = prompt("tut", "no");
+
+console.log(typeof(rar));
---
+++ script.js
@@ -0,0 +1,6 @@
+alert("ebat ty loh");
+
+var name = prompt( "Alex?", "");
+alert(name);
+
+setInterval(alert('pidor'), 1111)
